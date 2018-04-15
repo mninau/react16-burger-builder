c9144118d84a3c9fa143f6f127e0213b2cdd50a6
Marta Gaona Jiménez <mninau@gmail.com>
2018-04-15 21:10:50 +0200

    first component

--- complete-example/src/App.js
+++ complete-example/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Person from './Person/Person';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
         <p>lalalala</p>
+        <Person/>
       </div>
     );
   }
