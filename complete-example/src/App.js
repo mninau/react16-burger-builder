33252b5cb4d26b8f31e78a94f1d696745bc176fd
Marta Gaona Jiménez <mninau@gmail.com>
2018-04-16 19:14:23 +0200

    updating state with method

--- complete-example/src/App.js
+++ complete-example/src/App.js
@@ -9,19 +9,26 @@ class App extends Component {
       { name: 'Darth Vader' },
       { name: 'Darth Sidious' },
     ],
-  }
+  };
+
+  switchNameHandler = () => {
+    alert('Was clicked');
+    // DON'T USE THIS: this.state.specialName = 'Patata';
+    this.setState({ specialName: 'C3PO' });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
         <p>React is working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name="Han"/>
         <Person name="Leia">It's a trap!</Person>
         <Person name="Luke"/>
         <Person name={this.state.darkSide[0].name}/>
         <Person name={this.state.darkSide[1].name}/>
+        <Person name={this.state.specialName}/>
       </div>
     );
   }
