8c7ae2cbd623b855207bc9b29270eceab5824b5b
Marta Gaona Jiménez <mninau@gmail.com>
2018-04-16 19:01:56 +0200

    using state

--- complete-example/src/App.js
+++ complete-example/src/App.js
@@ -4,14 +4,24 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    darkSide: [
+      { name: 'Darth Vader' },
+      { name: 'Darth Sidious' },
+    ],
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello world!</h1>
         <p>React is working!</p>
+        <button>Switch Name</button>
         <Person name="Han"/>
-        <Person name="Leia">It's a pricess!</Person>
+        <Person name="Leia">It's a trap!</Person>
         <Person name="Luke"/>
+        <Person name={this.state.darkSide[0].name}/>
+        <Person name={this.state.darkSide[1].name}/>
       </div>
     );
   }
